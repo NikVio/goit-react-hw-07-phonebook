89adfadea28def2ade71f2e01dc0ab61cb7f9c82
NikVio <131026216+NikVio@users.noreply.github.com>
2023-12-29 17:40:44 +0200

    fixedRender

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,32 +1,14 @@
 import { Contacts } from './Contacts';
 import { ContactBox } from './ContactList.styled';
 import { useSelector } from 'react-redux';
-import {
-  selectContactsValue,
-  selectFilters,
-  selectVisibleContacts,
-} from 'components/Redux/selectors';
+import { selectContactsValue } from 'components/Redux/selectors';
 
 export const ContactList = () => {
   const contacts = useSelector(selectContactsValue);
 
-  const visible = useSelector(selectVisibleContacts);
-
-  const filter = useSelector(selectFilters);
-
-  const getVisibleContacts = () => {
-    if (filter === '') {
-      return contacts;
-    }
-
-    return visible;
-  };
-
-  const visibleContacts = getVisibleContacts();
-
   return (
     <ContactBox>
-      {visibleContacts.map(item => (
+      {contacts.map(item => (
         <li key={item.id}>
           <Contacts el={item} />
         </li>
